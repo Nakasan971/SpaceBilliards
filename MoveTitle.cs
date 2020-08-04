a933f229093e9eb575633a4121b5c4b618e3c2ee
Nakasan971 <47513102+Nakasan971@users.noreply.github.com>
2020-08-04 22:43:12 +0900

    Add files via upload

---
+++ MoveTitle.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveTitle : MonoBehaviour {
+    //TitleSceneへ移動
+    public void OnClick(){
+        SceneManager.LoadScene("Title");
+    }
+}
